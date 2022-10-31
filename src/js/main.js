1483c0f73da99439cc41b19bb8d70a592dfa61ac
xGod <xlinx@icloud.com>
2022-11-01 00:25:01 +0800

    update react-aframe uses issue.

--- src/js/main.js
+++ src/js/main.js
@@ -12,8 +12,19 @@ window.addEventListener("load", function(event) {
   const timer1000ms = setInterval(clock1000ms, 1000);
 });
 
+let updatePeriod=20,updatePeriod_count=updatePeriod;
+let progressBar=document.getElementById('realtimePeriodCount');
 function clock1000ms() {
-  new boxs().init({sense:'a-scene',POI:[Math.random()*5,0,0]});
+  // new boxs().init({sense:'a-scene',POI:[Math.random()*5,0,0]});
+
+  progressBar.ariaValueMax=updatePeriod;
+  progressBar.innerHTML=updatePeriod_count+"sec";
+  progressBar.ariaValueNow=updatePeriod_count;
+  progressBar.style.width=updatePeriod_count/updatePeriod*100+"%";
+  updatePeriod_count--;
+  if(updatePeriod_count==0){
+    updatePeriod_count=updatePeriod;
+  }
 }
 function printOSVar(){
   document.querySelector('a-scene').setAttribute('code-that-does-this', '');
